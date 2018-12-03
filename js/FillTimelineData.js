9d46fc125729e07aa1893f87d5df64f0965c0322
kjmj <kmorton15@gmail.com>
2018-12-03 15:18:00 +0100

    resized timeline blocks & fixed read-more buttons

--- js/FillTimelineData.js
+++ js/FillTimelineData.js
@@ -5,15 +5,47 @@ fetch('https://spreadsheets.google.com/feeds/list/1X1Sl-Ju4xaL6z6rp7Cua6dOoUKyPf
 
         if(data.feed.entry.hasOwnProperty(key)) {
 
-            let title = data.feed.entry[key].gsx$title.$t;
-            let description = data.feed.entry[key].gsx$desctiption.$t;
-            let date = data.feed.entry[key].gsx$date.$t;
-            let imageLink = data.feed.entry[key].gsx$imagelink.$t;
+            // Icon
+            let icon = document.createElement("img");
             let type = data.feed.entry[key].gsx$type.$t;
-            let readMoreLink = data.feed.entry[key].gsx$readmorelink.$t;
+            if(type == "IQP_Award") {
+                icon.src = "img/cd-icon-star.png";
+            } else if (type == "Other") {
+                icon.src = "img/cd-icon-towers.png";
+            } else if (type == "Pub") {
+                icon.src = "img/cd-icon-pub.png";
+            }
 
-            console.log('buttz', imageLink);
-            // create elements for the timeline block
+            // Title
+            let title = data.feed.entry[key].gsx$title.$t;
+            let h2 = document.createElement("h2");
+            h2.innerHTML = title;
+
+            // Description
+            let description = data.feed.entry[key].gsx$desctiption.$t;
+            let p = document.createElement("p");
+            p.innerHTML = description;
+
+            // Date
+            let date = data.feed.entry[key].gsx$date.$t;
+            let span = document.createElement("span");
+            span.className = "cd-timeline__date";
+            span.innerHTML = date;
+
+            // Read More button
+            let readMoreLink = data.feed.entry[key].gsx$readmorelink.$t;
+            let a = document.createElement("a");
+            a.href= readMoreLink;
+            a.target = "_blank";
+            a.className = "cd-timeline__read-more";
+            a.innerHTML = "Read More";
+
+            // Image
+            let imageLink = data.feed.entry[key].gsx$imagelink.$t;
+            let img = document.createElement("img");
+            img.src = imageLink;
+
+            // Create elements for the timeline block
             let div1 = document.createElement("div");
             div1.className = "cd-timeline__block js-cd-block";
 
@@ -23,35 +55,7 @@ fetch('https://spreadsheets.google.com/feeds/list/1X1Sl-Ju4xaL6z6rp7Cua6dOoUKyPf
             let div3 = document.createElement("div");
             div3.className = "cd-timeline__content js-cd-content";
 
-            let icon = document.createElement("img");
-            if(type == "IQP_Award") {
-                icon.src = "img/cd-icon-star.png";
-            } else if (type == "Other") {
-                icon.src = "img/cd-icon-towers.png";
-            } else if (type == "Pub") {
-                icon.src = "img/cd-icon-pub.png";
-            }
-
-            let h2 = document.createElement("h2");
-            h2.innerHTML = title;
-
-            let p = document.createElement("p");
-            p.innerHTML = description;
-
-            let span = document.createElement("span");
-            span.className = "cd-timeline__date";
-            span.innerHTML = date;
-
-            let a = document.createElement("a");
-            a.src = readMoreLink;
-            a.target = "_blank";
-            a.className = "cd-timeline__read-more";
-            a.innerHTML = "Read More";
-
-            let img = document.createElement("img");
-            img.src = imageLink;
-
-            // build the timeline block
+            // Construct the timeline block
             div2.appendChild(icon);
 
             div3.appendChild(h2);
@@ -59,7 +63,7 @@ fetch('https://spreadsheets.google.com/feeds/list/1X1Sl-Ju4xaL6z6rp7Cua6dOoUKyPf
                 div3.appendChild(img);
             }
             div3.appendChild(p);
-            if(a.src != "") {
+            if(readMoreLink != '') {
                 div3.appendChild(a);
             }
             div3.appendChild(span);
@@ -70,7 +74,6 @@ fetch('https://spreadsheets.google.com/feeds/list/1X1Sl-Ju4xaL6z6rp7Cua6dOoUKyPf
             document.getElementsByClassName("cd-timeline__container")[0].appendChild(div1);
         }
     }
-
 }).catch(err => {
     console.log(err);
 });
