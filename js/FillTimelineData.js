21c4e094ff86e2e4debb96b29922b8202ab54667
kjmj <kmorton15@gmail.com>
2018-12-04 20:06:05 +0100

    added filter & sort functionality

--- js/FillTimelineData.js
+++ js/FillTimelineData.js
@@ -1,49 +1,201 @@
 fetch('https://spreadsheets.google.com/feeds/list/1X1Sl-Ju4xaL6z6rp7Cua6dOoUKyPfPHbuRCo_-V7jaA/od6/public/values?alt=json').then(response => {
     return response.json();
-}).then(data => {
-    for(var key in data.feed.entry) {
+}).then(json => {
+    let data = json.feed.entry;
+    let filteredData = [];
+    let sortedData = [];
 
-        if(data.feed.entry.hasOwnProperty(key)) {
+    // Dropdown menus
+    let sortDropdown = createSortDropdown();
+    document.getElementById("dropdown").append(sortDropdown);
 
+    let filterDropdown = createFilterDropdown(getProjectTypes(data));
+    document.getElementById("dropdown").append(filterDropdown);
+
+    document.getElementById("filterDropdown").onchange = function() {
+        // var index = this.selectedIndex;
+        // var inputText = this.children[index].innerHTML.trim();
+
+        let value = this.value;
+        let sortValue = document.getElementById("sortDropdown").value;
+
+        filteredData = filterData(data, value);
+        sortedData = sortData(filteredData, sortValue);
+        console.log("Filtered by: " + value);
+        console.log("Sorted by: " + sortValue);
+        document.getElementsByClassName("cd-timeline__container")[0].innerHTML = "";
+        // document.getElementsByClassName("cd-timeline__container")[0].appendChild(sortDropdown);
+        // document.getElementsByClassName("cd-timeline__container")[0].appendChild(filterDropdown);
+
+        constructPage(filteredData);
+    };
+
+    document.getElementById("sortDropdown").onchange = function() {
+        // var index = this.selectedIndex;
+        // var inputText = this.children[index].innerHTML.trim();
+
+        let value = this.value;
+        let filterValue = document.getElementById("filterDropdown").value;
+
+        filteredData = filterData(data, filterValue);
+        sortedData = sortData(filteredData, value);
+        console.log("Filtered by: " + filterValue);
+        console.log("Sorted by: " + value);
+        document.getElementsByClassName("cd-timeline__container")[0].innerHTML = "";
+        // document.getElementsByClassName("cd-timeline__container")[0].appendChild(sortDropdown);
+        // document.getElementsByClassName("cd-timeline__container")[0].appendChild(filterDropdown);
+
+        constructPage(sortedData);
+    };
+
+}).catch(err => {
+    console.log(err);
+});
+
+function createSortDropdown() {
+    let dropdown = document.createElement("select");
+    dropdown.id = "sortDropdown";
+
+    let defaultOption = document.createElement("option");
+    defaultOption.selected = "selected";
+    defaultOption.disabled = "disabled";
+    defaultOption.innerHTML = "--- Sort By ---";
+
+    let option1 = document.createElement("option");
+    option1.value = "A to Z";
+    option1.text = "A to Z";
+
+    let option2 = document.createElement("option");
+    option2.value = "Z to A";
+    option2.text = "Z to A";
+
+    let option3 = document.createElement("option");
+    option3.value = "Old to New";
+    option3.text = "Old to New";
+
+    let option4 = document.createElement("option");
+    option4.value = "New to Old";
+    option4.text = "New to Old";
+
+    dropdown.options.add(defaultOption);
+    dropdown.options.add(option1);
+    dropdown.options.add(option2);
+    dropdown.options.add(option3);
+    dropdown.options.add(option4);
+
+    return dropdown;
+}
+
+/**
+ *
+ * @param filterItems An array of strings containing the items the user can filter by
+ * @returns {HTMLElement}
+ */
+function createFilterDropdown(filterItems) {
+    let dropdown = document.createElement("select");
+    // dropdown.selectionDisabled = "--- Filter By ---";
+    dropdown.id = "filterDropdown";
+
+    let defaultOption = document.createElement("option");
+    defaultOption.selected = "selected";
+    defaultOption.disabled = "disabled";
+    defaultOption.innerHTML = "--- Filter By ---";
+    dropdown.add(defaultOption);
+
+    let option = document.createElement("option");
+    option.value = "All";
+    option.textContent = "All";
+    dropdown.options.add(option);
+
+    for(item in filterItems) {
+        let option = document.createElement("option");
+        option.value = filterItems[item];
+        option.textContent = filterItems[item];
+        dropdown.options.add(option);
+    }
+
+    return dropdown;
+}
+
+function sortData(data, method) {
+
+    if(method == "Old to New") {
+        data.sort(function (a, b) {
+            return a.gsx$date.$t.localeCompare(b.gsx$date.$t);
+        });
+    } else if (method == "New to Old") {
+        data.sort(function (a, b) {
+            return b.gsx$date.$t.localeCompare(a.gsx$date.$t);
+        });
+    } else if (method == "A to Z") {
+        data.sort(function (a, b) {
+            return a.gsx$title.$t.localeCompare(b.gsx$title.$t);
+        });
+    } else if (method == "Z to A") {
+        data.sort(function (a, b) {
+            return b.gsx$title.$t.localeCompare(a.gsx$title.$t);
+        });
+    }
+
+    return data;
+}
+
+function constructPage(data) {
+    // Process data
+    for(var key in data) {
+
+        if(data.hasOwnProperty(key)) {
             // Icon
             let icon = document.createElement("img");
-            let type = data.feed.entry[key].gsx$type.$t;
+            let type = data[key].gsx$type.$t;
             if(type == "IQP_Award") {
                 icon.src = "img/cd-icon-star.png";
             } else if (type == "Other") {
                 icon.src = "img/cd-icon-towers.png";
-            } else if (type == "Pub") {
+            } else if (type == "Publication") {
                 icon.src = "img/cd-icon-pub.png";
             }
 
             // Title
-            let title = data.feed.entry[key].gsx$title.$t;
+            let title = data[key].gsx$title.$t;
             let h2 = document.createElement("h2");
             h2.innerHTML = title;
 
             // Description
-            let description = data.feed.entry[key].gsx$desctiption.$t;
+            let description = data[key].gsx$desctiption.$t;
             let p = document.createElement("p");
             p.innerHTML = description;
+            p.id = key;
 
             // Date
-            let date = data.feed.entry[key].gsx$date.$t;
+            let date = data[key].gsx$date.$t;
             let span = document.createElement("span");
             span.className = "cd-timeline__date";
             span.innerHTML = date;
 
             // Read More button
-            let readMoreLink = data.feed.entry[key].gsx$readmorelink.$t;
+            let readMoreLink = data[key].gsx$readmorelink.$t;
             let a = document.createElement("a");
             a.href= readMoreLink;
             a.target = "_blank";
             a.className = "cd-timeline__read-more";
             a.innerHTML = "Read More";
 
+            // Youtube Video
+            let youtubeLink = data[key].gsx$youtubelink.$t;
+            let iframe = document.createElement("iframe");
+            iframe.src = youtubeLink;
+            iframe.allowFullscreen = true;
+
             // Image
-            let imageLink = data.feed.entry[key].gsx$imagelink.$t;
+            let imageLink = data[key].gsx$imagelink.$t;
             let img = document.createElement("img");
             img.src = imageLink;
+
+            // Show/Hide Button
+            // let button = document.createElement("button");
+            // button.innerHTML = "Show/Hide";
+            // button.onclick = showAndHideBlock(p.id);
 
             // Create elements for the timeline block
             let div1 = document.createElement("div");
@@ -59,11 +211,16 @@ fetch('https://spreadsheets.google.com/feeds/list/1X1Sl-Ju4xaL6z6rp7Cua6dOoUKyPf
             div2.appendChild(icon);
 
             div3.appendChild(h2);
-            if(img.src != '') {
+            // div3.appendChild(button);
+            if(img.src !== '') {
                 div3.appendChild(img);
             }
+            if(youtubeLink !== '') {
+                console.log(iframe.src);
+                div3.appendChild(iframe);
+            }
             div3.appendChild(p);
-            if(readMoreLink != '') {
+            if(readMoreLink !== '') {
                 div3.appendChild(a);
             }
             div3.appendChild(span);
@@ -74,10 +231,26 @@ fetch('https://spreadsheets.google.com/feeds/list/1X1Sl-Ju4xaL6z6rp7Cua6dOoUKyPf
             document.getElementsByClassName("cd-timeline__container")[0].appendChild(div1);
         }
     }
-}).catch(err => {
-    console.log(err);
-});
+}
 
+function filterData(data, filterBy) {
+    if(filterBy == "All") {
+        return data;
+    } else{
+        let filtered = data.filter(x => x.gsx$type.$t === filterBy);
+        return filtered;
+    }
+}
 
-
-
+function getProjectTypes(data) {
+    let types = [];
+    for(var key in data) {
+        if (data.hasOwnProperty(key)) {
+            let type = data[key].gsx$type.$t;
+            if(!types.includes(type)) {
+                types.push(type);
+            }
+        }
+    }
+    return types;
+}
